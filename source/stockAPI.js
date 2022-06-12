5c6e85dfaf129235dcacf9fbc6004e642090fbae
duc-minh-droid <ducminhcsp@gmail.com>
2022-06-12 19:52:46 +0700

    setting up

---
+++ source/stockAPI.js
@@ -0,0 +1,8 @@
+const finnhub = require('finnhub');
+
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "caitkgaad3i2a9kciing" // Replace this
+const finnhubClient = new finnhub.DefaultApi()
+
+
+console.log(finnhubClient)
