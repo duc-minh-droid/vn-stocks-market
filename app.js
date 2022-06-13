65c2d935836085039858d42f08574112aa0f6835
duc-minh-droid <ducminhcsp@gmail.com>
2022-06-13 19:00:56 +0700

    last upd

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const API = require('./modules/stockAPI')
+
 const app = express();
 const port = 3000;
 
-app.get("/" , (req, res) => res.sendFile("./index.js",{root: __dirname}));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.get("/" , (req, res) => res.sendFile("./index.html",{root: __dirname}));
+app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
+
+console.log('API')
--- modules/stockAPI.js
+++ modules/stockAPI.js
@@ -1,8 +1,23 @@
 const finnhub = require('finnhub');
 
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
-api_key.apiKey = "caitkgaad3i2a9kciing" // Replace this
+api_key.apiKey = "sandbox_caj111qad3i94jmmsob0" // Replace this
 const finnhubClient = new finnhub.DefaultApi()
 
 
-module.exports = finnhubClient
+const dataAPI = finnhubClient.stockSymbols("VN", (error, data, response) => {
+    return data
+});
+
+const executiveAPI = finnhubClient.companyExecutive('AAPL', (error, data, response) => {
+    return data
+});
+
+const newsAPI = finnhubClient.marketNews("general", {}, (error, data, response) => {
+    return data
+})
+
+const transAPI = finnhubClient.insiderTransactions('AAPL', (error, data, response) => {
+    console.log(data);
+  });
+// module.exports = companyAPI
